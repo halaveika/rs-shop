009587a55273a10fe1f07c60e57ba82f3f1a499c
halaveika <halaveika1988@gmail.com>
2021-09-05 23:58:59 +0300

    feat: add headers components

---
+++ src/app/core/components/header/header-nav/basket-btn/basket-btn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basket-btn',
+  templateUrl: './basket-btn.component.html',
+  styleUrls: ['./basket-btn.component.scss']
+})
+export class BasketBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,29 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SharedModule } from '@shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { PromoListComponent } from './components/header/promo-list/promo-list.component';
+import { HeaderInfoComponent } from './components/header/header-info/header-info.component';
+import { HeaderNavComponent } from './components/header/header-nav/header-nav.component';
+import { LocationComponent } from './components/header/header-info/location/location.component';
+import { HeaderContactComponent } from './components/header/header-info/header-contact/header-contact.component';
+import { SearchComponent } from './components/header/header-nav/search/search.component';
+import { LoginBtnComponent } from './components/header/header-nav/login-btn/login-btn.component';
+import { BasketBtnComponent } from './components/header/header-nav/basket-btn/basket-btn.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PromoListComponent,
+    HeaderInfoComponent,
+    HeaderNavComponent,
+    LocationComponent,
+    HeaderContactComponent,
+    SearchComponent,
+    LoginBtnComponent,
+    BasketBtnComponent
   ],
   exports: [
     HeaderComponent,
